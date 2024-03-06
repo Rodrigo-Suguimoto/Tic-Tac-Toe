33c568509847fb1dba6bb8d13657c6c192b9f6ed
Rodrigo Suguimoto <rodrigo.suguimoto@MacBook-Pro.local>
2024-03-06 07:40:04 -0300

    refactored stage 2

--- Simple Tic-Tac-Toe (Java)/task/src/tictactoe/Main.java
+++ Simple Tic-Tac-Toe (Java)/task/src/tictactoe/Main.java
@@ -6,26 +6,14 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String userInput = scanner.next();
-        int gridLimit = 3;
-        System.out.println("---------"); // First divider
+        final int GRID_LIMIT = 9;
 
-        for (int i = 0; i < userInput.length(); i++) {
-            if (i == 0) { // Beginning of the grid
-                System.out.print("| ");
-                System.out.print(userInput.charAt(i) + " ");
-            } else if (i % gridLimit == 0) { // When i % 3 == 0, break the line and add a '|'
-                System.out.println();
-                System.out.print("| ");
-                System.out.print(userInput.charAt(i) + " ");
-            } else {
-                if (i == 2 || i == 5 | i == 8) { // If the i position is at the end of the line, add a '|' to signal the end of the line
-                    System.out.print(userInput.charAt(i) + " |");
-                } else {
-                    System.out.print(userInput.charAt(i) + " ");
-                }
-            }
+        System.out.println("---------"); // Print first divider
+
+        for (int i = 0; i < GRID_LIMIT; i += 3) {
+            System.out.printf("| %c %c %c |\n", userInput.charAt(i), userInput.charAt(i+1), userInput.charAt(i+2));
         }
-        System.out.println();
-        System.out.println("---------"); // Second divider
+
+        System.out.println("---------"); // Print second divider
     }
 }
