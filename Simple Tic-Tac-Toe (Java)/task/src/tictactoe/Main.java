e89c80c0d09a653adf5a6b2ebd166e9f013bdb71
Rodrigo Suguimoto <rodrigo.suguimoto@MacBook-Pro.local>
2024-03-13 07:15:20 -0300

    Advanced on third step - already finding winners on each column

--- Simple Tic-Tac-Toe (Java)/task/src/tictactoe/Main.java
+++ Simple Tic-Tac-Toe (Java)/task/src/tictactoe/Main.java
@@ -29,5 +29,48 @@ public class Main {
                 }
             }
         }
+
+        findWinner(ticTacToeMatrix);
+    }
+
+    public static String findWinner(char[][] ticTacToeMatrix) {
+        int counterXFirstColumn = 0;
+        int counterXSecondColumn = 0;
+        int counterXThirdColumn = 0;
+
+        int counterOFirstColumn = 0;
+        int counterOSecondColumn = 0;
+        int counterOThirdColumn = 0;
+
+        for (int row = 0; row < ticTacToeMatrix.length; row++) {
+            if (ticTacToeMatrix[row][0] == 'X') {
+                counterXFirstColumn++;
+            }
+            if (ticTacToeMatrix[row][1] == 'X') {
+                counterXSecondColumn++;
+            }
+            if (ticTacToeMatrix[row][2] == 'X') {
+                counterXThirdColumn++;
+            }
+
+            if (ticTacToeMatrix[row][0] == 'O') {
+                counterOFirstColumn++;
+            }
+            if (ticTacToeMatrix[row][1] == 'O') {
+                counterOSecondColumn++;
+            }
+            if (ticTacToeMatrix[row][2] == 'O') {
+                counterOThirdColumn++;
+            }
+        }
+
+        System.out.println("Counter X");
+        System.out.print(counterXFirstColumn + " " + counterXSecondColumn + " " + counterXThirdColumn);
+        System.out.println();
+
+        System.out.println("Counter O");
+        System.out.print(counterOFirstColumn + " " + counterOSecondColumn + " " + counterOThirdColumn);
+
+        return "winner";
     }
 }
